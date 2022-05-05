90487d5a3f8f2842a782543498b7bd16424d4bb7
Ivo Georgiev <ivo@ambire.com>
2022-05-05 14:38:40 +0300

    jsdoc updates

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const { ethers } = require('ethers')
 const VALIDATOR_1271_ABI = ['function isValidSignature(bytes32 hash, bytes signature) view returns (bytes4)']
 
 /**
- * @param provider Web3 Compatible provider to call deployed 1271 smart contracts (window.ethereum, web3.currentProvider, ethers provider... )
+ * @param provider Web3 Compatible provider to perform smart contract wallet validation with EIP 1271 (window.ethereum, web3.currentProvider, ethers provider... )
  * @param signer The signer address to verify the signature against
- * @param message To verify eth_sign type of signatures. Human message to verify. Message should be a human string or the hex version of the human string encoded as Uint8Bytes. If a hex string is passed, it will be considered as a regular string
+ * @param message To verify eth_sign type of signatures. Human-readable message to verify. Message should be a human string or the hex version of the human string encoded as Uint8Array. If a hex string is passed, it will be considered as a regular string
  * @param typedData To verify a 712 signature type. The {domain, type, message} 712 message object
- * @param finalDigest The final digest to verify. DApp will have to pre-compute the hash as no hashing transformation will occur and this digest will be directly used for recoverAddress and isValidSignature
- * @param signature The signature to verify
- * @param undeployedCallback An optional 1271 callback function to gracefully handle 1271 checks for undeployed contracts
+ * @param finalDigest The final digest to verify. dApp will have to pre-compute the hash as no hashing transformation will occur and this digest will be directly used for recoverAddress and isValidSignature
+ * @param signature The signature to verify as a hex string or Uint8Array
+ * @param undeployedCallback An optional 1271 callback function to gracefully handle signature validation for non-deployed smart contract wallets
  * @returns {Promise<{success: boolean, type: string}|{success: boolean}>}
+ * NOTE: you only need to pass one of: typedData, finalDigest, message
  */
 const  verifyMessage = async ({ provider, signer, message, typedData, finalDigest, signature, undeployedCallback }) => {
   if (message) {
