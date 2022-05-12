6df2398ab29fdb6240a9f726e930800e8c0b52f7
0xdex18 <iam.digital@mailnova.com>
2022-05-12 20:53:12 +0200

    Adding types to jsdoc params + adding test for uint8Arr signature

---
+++ tests/7_ethSign_uint8Signature.js
@@ -0,0 +1,23 @@
+const test = require('tape')
+const ethers = require('ethers')
+const { RPC, MNEMONIC } = require('../testConfig')
+const { verifyMessage } = require('../index')
+
+test('eth_sign, signature as uint8Arr verification', async function (t) {
+
+  const provider = new ethers.providers.JsonRpcProvider(RPC.polygon)
+  const signer = ethers.Wallet.fromMnemonic(MNEMONIC)
+  const hexxed = '0x123456'
+  const signature = await signer.signMessage(ethers.utils.arrayify(hexxed))
+
+  return verifyMessage({
+    signer: signer.address,
+    provider,
+    message: ethers.utils.arrayify(hexxed),
+    signature: ethers.utils.arrayify(signature),
+  }).then(result => {
+    t.assert(result, 'Valid signature')
+  }).catch(e => {
+    t.error(e, 'Invalid signature')
+  })
+})
