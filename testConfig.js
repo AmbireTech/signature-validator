812f8f5f3332dbb878f2a215623a7bff6cef5144
Ivo Georgiev <ivo@ambire.com>
2025-08-27 15:16:44 -0700

    fix tests by using a random mnemonic: closes #13

--- testConfig.js
+++ testConfig.js
@@ -48,8 +48,7 @@ module.exports = {
   RPC: {
     polygon: 'https://polygon-rpc.com',
   },
-  MNEMONIC:
-    'radar blur cabbage chef fix engine embark joy scheme fiction master release',
+  MNEMONIC: ethers.utils.entropyToMnemonic(ethers.utils.randomBytes(32)),
   defaultPublicClient: createPublicClient({
     chain: polygon,
     transport: http('https://polygon-rpc.com'),
